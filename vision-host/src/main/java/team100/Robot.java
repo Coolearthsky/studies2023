e15c8e6d6a66dfd6eeab5295320174c2cfd7fdf4
Joel Truher <joel@truher.org>
2023-01-27 12:28:28 -0800

    NT4 vision subscriber works

--- vision-host/src/main/java/team100/Robot.java
+++ vision-host/src/main/java/team100/Robot.java
@@ -1,7 +1,21 @@
 package team100;
 
+import java.io.IOException;
+import java.util.EnumSet;
+
+import org.msgpack.jackson.dataformat.MessagePackFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -13,11 +27,48 @@ import edu.wpi.first.wpilibj.Timer;
 public class Robot extends TimedRobot {
   private final DoublePublisher timestamp_publisher;
 
+  private final ObjectMapper object_mapper;
+
   public Robot() {
     NetworkTableInstance inst = NetworkTableInstance.getDefault();
     inst.startServer("example server");
-    NetworkTable table = inst.getTable("example_table");
-    timestamp_publisher = table.getDoubleTopic("timestamp").publish();
+    NetworkTable example_table = inst.getTable("example_table");
+    timestamp_publisher = example_table.getDoubleTopic("timestamp").publish();
+    object_mapper = new ObjectMapper(new MessagePackFactory());
+    NetworkTable vision_table = inst.getTable("Vision");
+    inst.addListener(
+        vision_table.getEntry("tags"),
+        EnumSet.of(NetworkTableEvent.Kind.kValueAll),
+        (event) -> accept(event));
+  }
+
+  private void accept(NetworkTableEvent event) {
+    try {
+      Blips blips = object_mapper.readValue(event.valueData.value.getRaw(), Blips.class);
+      System.out.printf("PAYLOAD %s\n", blips);
+      System.out.printf("DELAY (s): %f\n", blips.et);
+      System.out.printf("BLIP COUNT: %d\n", blips.tags.size());
+      for (Blip b: blips.tags ) {
+        Pose3d p = blipToPose(b);
+        System.out.printf("TAG ID: %d\n", b.id);
+        System.out.printf("POSE: %s\n", p);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private Pose3d blipToPose(Blip b) {
+    Translation3d t = new Translation3d(b.pose_t[0][0], b.pose_t[1][0], b.pose_t[2][0]);
+    Matrix<N3, N3> rot = new Matrix<N3, N3>(Nat.N3(), Nat.N3());
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        rot.set(i,j,b.pose_R[i][j]);
+      }
+    }
+    Rotation3d r = new Rotation3d(rot);
+    Pose3d p = new Pose3d(t, r);
+    return p;
   }
 
   @Override
