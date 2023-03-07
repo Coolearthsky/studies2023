d9115d33e8afdbcd6fcf147f58f462379939143f
Joel Truher <joel@truher.org>
2023-03-07 12:28:47 -0800

    working on multi-function indicator

--- led_example/src/main/java/frc/robot/Robot.java
+++ led_example/src/main/java/frc/robot/Robot.java
@@ -1,16 +1,22 @@
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import team100.GoNoGoIndicator;
+import team100.OnboardIndicator;
 
 public class Robot extends TimedRobot {
-    GoNoGoIndicator indicator;
+    OnboardIndicator indicator;
     XboxController controller;
+    ADXRS450_Gyro gyro; // for simulation
 
     public Robot() {
-        indicator = new GoNoGoIndicator(8);
+        indicator = new OnboardIndicator(8);
         controller = new XboxController(0);
+        gyro = new ADXRS450_Gyro();
     }
 
     @Override
@@ -20,6 +26,11 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotPeriodic() {
+        double headingDegreesNWU = -1.0 * gyro.getAngle();
+        double headingRadiansNWU = Units.degreesToRadians(headingDegreesNWU);
+        double wrappedHeadingRadiansNWU = MathUtil.angleModulus(headingRadiansNWU);
+        Rotation2d heading = new Rotation2d(wrappedHeadingRadiansNWU);
+        indicator.setHeading(heading);
         if (controller.getAButton())
             indicator.go();
         else if (controller.getBButton())
