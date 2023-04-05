8da9ce90a090038fbc202102f41ae6f7645f9640
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2023-04-05 09:24:42 -0700

    arm state space changes for bigger arm

--- arm_stage_space_control/src/main/java/frc/robot/Robot.java
+++ arm_stage_space_control/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxRelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.VecBuilder;
@@ -17,14 +21,7 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.AnalogEncoder;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
@@ -33,24 +30,23 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
  */
 public class Robot extends TimedRobot {
   private CommandXboxController controller0 = new CommandXboxController(1);
+  double wrappedPos;
   double setpoint;
-  private static final int kJoystickPort = 1;
-  private static final double kRaisedPosition = Units.degreesToRadians(90.0);
   private static final double kLoweredPosition = Units.degreesToRadians(0.0);
 
   // Moment of inertia of the arm, in kg * m^2. Can be estimated with CAD. If
   // finding this constant
   // is difficult, LinearSystem.identifyPositionSystem may be better.
-  private static final double kArmMOI = 0.004725;
+  private static final double kArmMOI = 0.0861125;
 
   // Reduction between motors and encoder, as output over input. If the arm spins
   // slower than
   // the motors, this number should be greater than one.
-  private static final double kArmGearing = 1;
+  private static final double kArmGearing = 15;
 
   private final TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(
-      Units.degreesToRadians(45),
-      Units.degreesToRadians(90)); // Max arm speed and acceleration.
+      12*Math.PI,
+      12*Math.PI); // Max arm speed and acceleration.
   private TrapezoidProfile.State m_lastProfiledReference = new TrapezoidProfile.State();
 
   // The plant holds a state-space model of our arm. This system has the following
@@ -60,7 +56,7 @@ public class Robot extends TimedRobot {
   // Inputs (what we can "put in"): [voltage], in volts.
   // Outputs (what we can measure): [position], in radians.
   private final LinearSystem<N2, N1, N1> m_armPlant = LinearSystemId
-      .createSingleJointedArmSystem(DCMotor.getVex775Pro(1), kArmMOI, kArmGearing);
+      .createSingleJointedArmSystem(DCMotor.getNEO(1), kArmMOI, kArmGearing);
 
   // The observer fuses our encoder data and voltage inputs to reject noise.
   private final KalmanFilter<N2, N1, N1> m_observer = new KalmanFilter<>(
@@ -76,7 +72,7 @@ public class Robot extends TimedRobot {
   // A LQR uses feedback to create voltage commands.
   private final LinearQuadraticRegulator<N2, N1, N1> m_controller = new LinearQuadraticRegulator<>(
       m_armPlant,
-      VecBuilder.fill(Units.degreesToRadians(1.0), Units.degreesToRadians(10.0)), // qelms.
+      VecBuilder.fill(Units.degreesToRadians(0.1), Units.degreesToRadians(10.0)), // qelms.
       // Position and velocity error tolerances, in radians and radians per second.
       // Decrease
       // this
@@ -85,7 +81,7 @@ public class Robot extends TimedRobot {
       // velocity, but
       // this
       // can be tuned to balance the two.
-      VecBuilder.fill(20), // relms. Control effort (voltage) tolerance. Decrease this to more
+      VecBuilder.fill(12), // relms. Control effort (voltage) tolerance. Decrease this to more
       // heavily penalize control effort, or make the controller less aggressive. 12
       // is a good
       // starting point because that is the (approximate) maximum voltage of a
@@ -99,31 +95,35 @@ public class Robot extends TimedRobot {
       0.020);
 
   // An encoder set up to measure arm position in radians.
-  private final Encoder m_encoder = new Encoder(8, 9);
-
-  private final PWMMotorController m_motor = new VictorSP(0);
+  private final CANSparkMax m_motor = new CANSparkMax(13, MotorType.kBrushless);
+  private final RelativeEncoder encoder = m_motor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
 
   // A joystick to read the trigger from.
-  private final Joystick m_joystick = new Joystick(kJoystickPort);
-
   @Override
   public void robotInit() {
     // We go 2 pi radians in 1 rotation, or 4096 counts.
-    m_encoder.setDistancePerPulse(Math.PI * 2 / 1024.0);
   }
 
   @Override
   public void teleopInit() {
-    // Reset our loop to make sure it's in a known state.
-    m_loop.reset(VecBuilder.fill(m_encoder.getDistance(), m_encoder.getRate()));
+    encoder.setPosition(0);
+    encoder.setPositionConversionFactor(2*Math.PI/15);
+    encoder.setVelocityConversionFactor(Math.PI/450);
 
-    // Reset our last reference to the current state.
-    m_lastProfiledReference = new TrapezoidProfile.State(m_encoder.getDistance(), m_encoder.getRate());
+    // Reset our loop to make sure it's in a known state.
+    m_loop.reset(
+        VecBuilder.fill(wrappedPos, encoder.getVelocity()));
+
+    // Reset our last re ference to the current state.
+    m_lastProfiledReference = new TrapezoidProfile.State(wrappedPos,
+        encoder.getVelocity());
   }
 
   @Override
   public void teleopPeriodic() {
-    setpoint = Math.atan2(MathUtil.applyDeadband(controller0.getLeftX(), 0.05), MathUtil.applyDeadband(controller0.getLeftY(), 0.05));
+    wrappedPos = MathUtil.angleModulus(encoder.getPosition());
+    setpoint = Math.atan2(MathUtil.applyDeadband(controller0.getLeftX(), 0.05),
+        MathUtil.applyDeadband(controller0.getLeftY(), 0.05));
     // Sets the target position of our arm. This is similar to setting the setpoint
     // of a
     // PID controller.
@@ -141,7 +141,7 @@ public class Robot extends TimedRobot {
     m_loop.setNextR(m_lastProfiledReference.position, m_lastProfiledReference.velocity);
 
     // Correct our Kalman filter's state vector estimate with encoder data.
-    m_loop.correct(VecBuilder.fill(m_encoder.getDistance()));
+    m_loop.correct(VecBuilder.fill(wrappedPos));
 
     // Update our LQR to generate new voltage commands and use xthe voltages to
     // predict the next
@@ -152,8 +152,16 @@ public class Robot extends TimedRobot {
     // voltage = duty cycle * battery voltage, so
     // duty cycle = voltage / battery voltage
     double nextVoltage = m_loop.getU(0);
-    double feedfordward = 2*Math.sin(m_encoder.getDistance());
-    System.out.printf("%8.3f %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f\n", nextVoltage, m_encoder.getRate(), m_encoder.getDistance(), feedfordward, setpoint, -controller0.getLeftY(), controller0.getLeftX());
-      m_motor.setVoltage(-1.0 * ( nextVoltage + feedfordward));
-  } 
+    double kS = 0.03 * Math.signum(nextVoltage);
+    // double kV = Math.sin(wrappedPos);
+    System.out.printf("v: %8.3f p: %8.3f v: %8.3f s: %8.3f y: %8.3f x: %8.3f\n",
+        nextVoltage,
+        wrappedPos,
+        encoder.getVelocity(),
+        //feedfordward,
+        setpoint,
+        -controller0.getLeftY(),
+        controller0.getLeftX());
+    m_motor.setVoltage(nextVoltage+kS);
+  }
 }
