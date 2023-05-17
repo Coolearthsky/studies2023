b8d9d4c36d62d003d9f83ad5030fc70ef4fe1339
Joel Truher <joel@truher.org>
2023-05-16 17:04:23 -0700

    clarifying and distancing from EKF itself

--- lqr_with_angles/src/test/java/team100/AngleControllerTest.java
+++ lqr_with_angles/src/test/java/team100/AngleControllerTest.java
@@ -8,24 +8,20 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
-import edu.wpi.first.math.controller.LinearQuadraticRegulator;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 
-public abstract class KFTestBase {
-
+public class AngleControllerTest {
     static final double kDelta = 0.001;
     static final double kDt = 0.02;
-
-    // MODEL
 
     // state: x is (angle (rad), angular_velocity (rad/s))
     final Nat<N2> states = Nat.N2();
     // control input: u is (volts)
     final Nat<N1> inputs = Nat.N1();
-    // measurement output: y is (angle)
-    final Nat<N1> outputs = Nat.N1();
+    // measurement output: y is (angle,velocity)
+    final Nat<N2> outputs = Nat.N2();
     // A: angle derivative is velocity
     final Matrix<N2, N2> A = Matrix.mat(states, states)
             .fill(0, 1, //
@@ -34,37 +30,20 @@ public abstract class KFTestBase {
     final Matrix<N2, N1> B = Matrix.mat(states, inputs)
             .fill(0, //
                     1);
-    // C: measurement output is angle
-    final Matrix<N1, N2> C = Matrix.mat(outputs, states)
-            .fill(1, 0);
+    // C: measurement output is angle,velocity
+    final Matrix<N2, N2> C = Matrix.mat(outputs, states)
+            .fill(1, 0, 0, 1);
     // D: control input does not affect measurement directly
-    final Matrix<N1, N1> D = Matrix.mat(outputs, inputs)
-            .fill(0);
-    final LinearSystem<N2, N1, N1> plant = new LinearSystem<>(A, B, C, D);
-
-    // OBSERVER
-    // observers are in subclasses; they don't share a superclass.  :-(
-
-    // Q: state stdev
-    final Vector<N2> stateStdDevs = VecBuilder.fill(0.015, 0.17);
-    // R: measurement stdev
-    final Vector<N1> measurementStdDevs = VecBuilder.fill(0.01);
-
-    // CONTROLLER
-
+    final Matrix<N2, N1> D = Matrix.mat(outputs, inputs)
+            .fill(0, 0);
+    final LinearSystem<N2, N1, N2> plant = new LinearSystem<>(A, B, C, D);
     final Vector<N2> stateTolerance = VecBuilder
             .fill(0.01, // angle (rad)
                     0.2); // velocity (rad/s)
     final Vector<N1> controlTolerance = VecBuilder
             .fill(12.0); // output (volts)
 
-    final LinearQuadraticRegulator<N2, N1, N1> controller;
-
-    public KFTestBase() {
-        controller = newController();
-    }
-
-    abstract LinearQuadraticRegulator<N2, N1, N1> newController();
+    final AngleController controller = new AngleController(plant, stateTolerance, controlTolerance, kDt);
 
     @Test
     public void testController() {
