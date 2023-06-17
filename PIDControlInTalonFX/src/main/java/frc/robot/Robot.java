0179e011c00e829031aedec162d2ce7cb2d597b3
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2023-06-17 11:28:28 -0700

    More comment changes

--- PIDControlInTalonFX/src/main/java/frc/robot/Robot.java
+++ PIDControlInTalonFX/src/main/java/frc/robot/Robot.java
@@ -36,16 +36,17 @@ public class Robot extends TimedRobot {
     //F is feedforward, none used for this test
     m_motor.config_kF(0, 0);
     //The P value you have for position control changes, but 0.25 was good for me. I also found having a D value helped.
-    //The P value for velocity seems to affect how fast the motor will spin, so if the P value is smaller, the motor will go slow multiplier of how much lower the P value is. My P value I used was 0.0001. However, this allows for much more control
-    m_motor.config_kP(0, 0.25); // V 0.0001
-    m_motor.config_kI(0, 0); // V 0
-    m_motor.config_kD(0, 20); // V 0
+    //The P value for velocity seems to affect how fast the motor will spin, so if the P value is smaller, the motor will go slower by a multiplier of how much lower the P value is. My P value I used was 0.0001. The lower P value allows the motor to not go crazy, this allows for much more control.
+    m_motor.config_kP(0, 0.0001); // P 0.25
+    m_motor.config_kI(0, 0); // P 0
+    m_motor.config_kD(0, 0); // P 20
     // int absolutePosition = 0;
     // m_motor.setSelectedSensorPosition(absolutePosition, 0, 30);
   }
 
   @Override
-  public void robotPeriodic() {
+  public void 
+  robotPeriodic() {
     CommandScheduler.getInstance().run();
   }
 
@@ -85,16 +86,16 @@ public class Robot extends TimedRobot {
     System.out.println(m_motor.get());
     //The gear ratio of the motor, of the motor spins less than the device this value should be greater than one
     double motorGearing = 6.6;
-    //There is a set value for the velocity that depends on the P value of the pid controller
+    //There is a set value for the velocity that depends on the P value of the pid controller P Constant = 10/P Value
     double speedConstant = 100000;
     // Units are rots per sec
-    double desiredVelocity = 0.5;
+    double desiredVelocity = 1;
     m_motor.set(ControlMode.Velocity, desiredVelocity * speedConstant * motorGearing);
     //For the talon FX encoder there were 2048 rots per rev, please check to see what yours is.
     int rotationsPerRev = 2048;
     //Units are rotations
     double desiredPosition = 1;
-    m_motor.set(ControlMode.Position, desiredPosition * rotationsPerRev * motorGearing);
+    // m_motor.set(ControlMode.Position, desiredPosition * rotationsPerRev * motorGearing);
   }
 
   @Override
