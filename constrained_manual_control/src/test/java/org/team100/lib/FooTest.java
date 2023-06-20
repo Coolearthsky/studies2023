49d01db2987e1158a7d70dad244550dd3df2f1f3
Joel Truher <joel@truher.org>
2023-06-20 08:46:35 -0700

    initial commit

---
+++ constrained_manual_control/src/test/java/org/team100/lib/FooTest.java
@@ -0,0 +1,14 @@
+package org.team100.lib;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+    @Test
+    public void testNothing() {
+        Foo foo = new Foo();
+        assertTrue(true);
+    }
+    
+}
