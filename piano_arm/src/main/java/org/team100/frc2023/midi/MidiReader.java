0a16f1832a92bfb18c8ca3bd52234e64f81e5908
Joel Truher <joel@truher.org>
2023-07-27 09:13:10 -0700

    midi files and reader

---
+++ piano_arm/src/main/java/org/team100/frc2023/midi/MidiReader.java
@@ -0,0 +1,86 @@
+package org.team100.frc2023.midi;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+/**
+ * Read a MIDI file.
+ * I don't use the java sequencer, I just want the timestamped events.
+ */
+public class MidiReader {
+    private final Track[] m_tracks;
+
+    public MidiReader() {
+        this("src/main/deploy/head_over_heels_duet.mid");
+    }
+
+    public MidiReader(String filepath) {
+        m_tracks = makeTracks(filepath);
+    }
+
+    public Track[] getTracks() {
+        return m_tracks;
+    }
+
+    /** Dumps all the tracks to stdout. */
+    public void print() {
+        for (Track track : m_tracks) {
+            for (int iEvent = 0; iEvent < track.size(); ++iEvent) {
+                MidiEvent event = track.get(iEvent);
+                MidiMessage msg = event.getMessage();
+                long tick = event.getTick();
+                if (!(msg instanceof ShortMessage)) {
+                    System.out.println("Other message: " + msg.getClass());
+                    continue;
+                }
+                ShortMessage sm = (ShortMessage) msg;
+
+                if (sm.getCommand() != ShortMessage.NOTE_ON) {
+                    System.out.println("Command:" + sm.getCommand());
+                    continue;
+                }
+
+                // musescore 4 doesn't do channel assignment
+                // https://musescore.org/en/node/339580
+                // so i'll have to have a little map
+                int channel = sm.getChannel();
+                int key = sm.getData1();
+                int velocity = sm.getData2();
+                if (velocity > 0) {
+                    System.out.println(
+                            "Ch " + channel + " tick " + tick + " Attack  " + " key " + key + " v " + velocity);
+                } else {
+                    System.out.println(
+                            "Ch " + channel + " tick " + tick + " Release " + " key " + key);
+                }
+            }
+        }
+    }
+
+    /** Returns an array of tracks, empty if something goes wrong. */
+    private Track[] makeTracks(String filepath) {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            InputStream is = new BufferedInputStream(new FileInputStream(new File(filepath)));
+            sequencer.setSequence(is);
+            Sequence sequence = sequencer.getSequence();
+            return sequence.getTracks();  
+        } catch (MidiUnavailableException | IOException | InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+        return new Track[0];
+    }
+}
