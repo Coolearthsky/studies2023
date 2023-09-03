9ba8f515b8d96de991b236dfa93ac05e30b3c57d
Joel Truher <joel@truher.org>
2023-09-03 09:43:29 -0700

    add parameters

--- rrts/src/test/java/org/team100/lib/graph/OperationsTest.java
+++ rrts/src/test/java/org/team100/lib/graph/OperationsTest.java
@@ -50,32 +50,32 @@ public class OperationsTest {
         assertNull(root.getIncoming());
 
         Node<N1> node1 = new Node<>(x);
-        LinkInterface link1 = Graph.newLink(root, node1, 1);
+        LinkInterface<N1> link1 = Graph.newLink(root, node1, 1);
         assertEquals(root, node1.getIncoming().get_source());
         assertEquals(1, link1.get_linkDist(), 0.1);
         assertEquals(1, link1.get_pathDist(), 0.1);
 
         Node<N1> node2 = new Node<>(x);
-        LinkInterface link2 = Graph.newLink(node1, node2, 1);
+        LinkInterface<N1> link2 = Graph.newLink(node1, node2, 1);
 
         assertEquals(node1, node2.getIncoming().get_source());
         assertEquals(1, link2.get_linkDist(), 0.1);
         assertEquals(2, link2.get_pathDist(), 0.1);
 
         Node<N1> node3 = new Node<>(x);
-        LinkInterface link3 = Graph.newLink(node2, node3, 1);
+        LinkInterface<N1> link3 = Graph.newLink(node2, node3, 1);
         assertEquals(node2, node3.getIncoming().get_source());
         assertEquals(1, link3.get_linkDist(), 0.1);
         assertEquals(3, link3.get_pathDist(), 0.1);
 
         Node<N1> node4 = new Node<>(x);
-        LinkInterface link4 = Graph.newLink(node3, node4, 1);
+        LinkInterface<N1> link4 = Graph.newLink(node3, node4, 1);
         assertEquals(node3, node4.getIncoming().get_source());
         assertEquals(1, link4.get_linkDist(), 0.1);
         assertEquals(4, link4.get_pathDist(), 0.1);
 
         Node<N1> node5 = new Node<>(x);
-        LinkInterface link5 = Graph.newLink(node4, node5, 1);
+        LinkInterface<N1> link5 = Graph.newLink(node4, node5, 1);
         assertEquals(node4, node5.getIncoming().get_source());
         assertEquals(1, link5.get_linkDist(), 0.1);
         assertEquals(5, link5.get_pathDist(), 0.1);
@@ -84,13 +84,13 @@ public class OperationsTest {
         Graph.rewire(myRobot, node2, node4, 1);
 
         // the pathdist of that node is updated
-        LinkInterface newLink4 = node4.getIncoming();
+        LinkInterface<N1> newLink4 = node4.getIncoming();
         assertEquals(node2, newLink4.get_source());
         assertEquals(1, newLink4.get_linkDist(), 0.1);
         assertEquals(3, newLink4.get_pathDist(), 0.1);
 
         // make sure the subtree is also updated
-        LinkInterface newLink5 = node5.getIncoming();
+        LinkInterface<N1> newLink5 = node5.getIncoming();
         assertEquals(node4, newLink5.get_source());
         assertEquals(1, newLink5.get_linkDist(), 0.1);
         assertEquals(4, newLink5.get_pathDist(), 0.1);
