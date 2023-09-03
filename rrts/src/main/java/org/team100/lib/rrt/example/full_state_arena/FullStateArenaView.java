92051d405864d941d3021c7a09407c6a1168c530
Joel Truher <joel@truher.org>
2023-09-02 17:44:39 -0700

    removed extra classes in presentation layer

--- rrts/src/main/java/org/team100/lib/rrt/example/full_state_arena/FullStateArenaView.java
+++ rrts/src/main/java/org/team100/lib/rrt/example/full_state_arena/FullStateArenaView.java
@@ -1,6 +1,7 @@
 package org.team100.lib.rrt.example.full_state_arena;
 
 import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
@@ -10,12 +11,15 @@ import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
+import java.lang.reflect.InvocationTargetException;
 import java.text.NumberFormat;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 import org.team100.lib.example.Arena;
 import org.team100.lib.geom.Obstacle;
@@ -24,7 +28,10 @@ import org.team100.lib.graph.Node;
 import org.team100.lib.index.KDNode;
 import org.team100.lib.index.KDTree;
 import org.team100.lib.planner.Runner;
+import org.team100.lib.planner.Solver;
+import org.team100.lib.rrt.RRTStar6;
 import org.team100.lib.space.Path;
+import org.team100.lib.space.Sample;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.numbers.N1;
@@ -55,6 +62,54 @@ public class FullStateArenaView extends JComponent {
         _T_a = T_a;
         _T_b = T_b;
     }
+
+
+
+    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
+        final FullStateHolonomicArena arena = new FullStateHolonomicArena();
+        KDNode<Node<N4>> T_a = new KDNode<>(new Node<>(arena.initial()));
+        KDNode<Node<N4>> T_b = new KDNode<>(new Node<>(arena.goal()));
+        final Solver<N4> solver = new RRTStar6<>(arena, new Sample<>(arena), 3, T_a, T_b);
+        final Runner<N4> runner = new Runner<>(solver);
+        final FullStateArenaView view = new FullStateArenaView(arena, runner, T_a, T_b);
+
+
+        // final JFrame frame = new FullStateArenaFrame(view);
+        final JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(view);
+
+
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                frame.setSize(1600, 800);
+                frame.setVisible(true);
+                frame.repaint();
+            }
+        });
+
+        runner.runForDurationMS(100000);
+        //runner.runSamples(5000);
+
+        Path<N4> bestPath = runner.getBestPath();
+        if (bestPath == null) {
+            System.out.println("failed to find path");
+        } else {
+            System.out.println("found path");
+            System.out.println(bestPath);
+        }
+        System.out.println("done");
+        frame.repaint();
+        view.repaint();
+
+    }
+
+
+
+
+
 
     @Override
     protected void paintComponent(Graphics graphics) {
